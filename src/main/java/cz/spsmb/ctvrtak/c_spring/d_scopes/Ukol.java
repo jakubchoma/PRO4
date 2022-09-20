ac88b6fd24f07a327c69976e9fd5bc8354f2aa0e
stemberk <stemberk@spsmb.cz>
2022-09-20 12:07:01 +0200

    ukol+

--- src/main/java/cz/spsmb/ctvrtak/c_spring/d_scopes/Ukol.java
+++ src/main/java/cz/spsmb/ctvrtak/c_spring/d_scopes/Ukol.java
@@ -4,6 +4,10 @@ package cz.spsmb.ctvrtak.c_spring.d_scopes;
  * Vytvořte test testIsPrototype()
  * tak, aby při zadání daného beanu test prošel, pokud se instance
  * dvojího volání metody getBean("beanId") liší.
+ *
+ * Vytvořte test testIsSingleton()
+ * tak, aby při zadání daného beanu test prošel, instance
+ * dvojího volání metody getBean("beanId") jsou stejné.
  */
 public class Ukol {
 }
