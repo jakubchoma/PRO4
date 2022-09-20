b9b009c6c7b5f88e397be51e1310479455100801
stemberk <stemberk@spsmb.cz>
2022-09-20 11:54:57 +0200

    ukol+

---
+++ src/main/java/cz/spsmb/ctvrtak/c_spring/d_scopes/Ukol.java
@@ -0,0 +1,9 @@
+package cz.spsmb.ctvrtak.c_spring.d_scopes;
+
+/***
+ * Vytvořte test testIsPrototype()
+ * tak, aby při zadání daného beanu test prošel, pokud se instance
+ * dvojího volání metody getBean("beanId") liší.
+ */
+public class Ukol {
+}
