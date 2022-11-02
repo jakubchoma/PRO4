88993e74600102bbe72a134fb98ac54ee31b67cf
stemb <stemberk@spsmb.cz>
2022-11-02 18:01:28 +0100

    prace s DB +

--- src/cz/spsmb/ctvrtak/d_maturitni_okruhy/v_prace_s_db/ASQLiteEx.java
+++ src/cz/spsmb/ctvrtak/d_maturitni_okruhy/v_prace_s_db/ASQLiteEx.java
@@ -2,6 +2,9 @@ package cz.spsmb.ctvrtak.d_maturitni_okruhy.v_prace_s_db;
 
 import java.sql.*;
 
+/***
+ * Do pom.xml je nutné přidat artefakt sqlite-jdbc (ovladač sqlite)
+ */
 public class ASQLiteEx {
     public static void main(String[] args) throws SQLException {
         Statement stmt;
@@ -9,7 +12,7 @@ public class ASQLiteEx {
         String url = "jdbc:sqlite:moje.db";
         Connection conn = DriverManager.getConnection(url);
 
-        // vytvoření tabulky
+        // vytvoření tabulky ((C)RUD - Create)
         stmt = conn.createStatement();
         String sql = "CREATE TABLE IF NOT EXISTS mojetabulka " +
                 "(id INTEGER PRIMARY KEY AUTOINCREMENT," +
@@ -17,12 +20,12 @@ public class ASQLiteEx {
         stmt.executeUpdate(sql);
         stmt.close();
 
-        //naplnění daty
+        //naplnění daty ((C)RUD -Create)
         PreparedStatement pstmt = conn.prepareStatement("INSERT INTO mojetabulka (name) VALUES (?)");
         pstmt.setString(1, "první záznam do DB");
         pstmt.executeUpdate();
 
-        //čtení dat
+        //čtení dat (C(R)UD - Read)
         stmt = conn.createStatement();
         ResultSet rs = stmt.executeQuery("SELECT * FROM mojetabulka;");
         String format = "%3s|%20s\n";
