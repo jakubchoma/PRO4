7b96644bd23fa63914cb8692afbb50a65db365c1
stemberk <stemberk@spsmb.cz>
2023-01-09 10:40:37 +0100

    pop +

---
+++ src/cz/spsmb/ctvrtak/f_pop/a_birthdate_reminder/AUvod.java
@@ -0,0 +1,11 @@
+package cz.spsmb.ctvrtak.f_pop.a_birthdate_reminder;
+
+/**
+ * Vytvořte aplikaci v JavaFX s návrhovým vzorem Model-View-Presenter (MVP), která umožní:
+ * - zadat osobu (jméno, datum narození a volitelně datum jmenin)
+ * - vypsat všechny zadané osoby (nejlépe pomocí komponenty TableView)
+ * - aplikace bude vždy zobrazovat pro aktuální týden 7 dní s případnými zadanými
+ *   jmény pro aktuální datum. Aktuální datum přitom půjde změnit za libovolný jiný.
+ */
+public class AUvod {
+}
---
+++ src/cz/spsmb/ctvrtak/f_pop/b_sudoku_game/AUvod.java
@@ -0,0 +1,11 @@
+package cz.spsmb.ctvrtak.f_pop.b_sudoku_game;
+
+/**
+ * Vytvořte pomocí JavaFX a nejlépe komponenty GridPane hru sudoku, která umí následující:
+ * - vytvořit herní plán
+ * - provést kontrolu (validaci) hráčem vyplněného plánu
+ * - Hra sudoku obsahuje 9x9 políček, počáteční čísla v nich jsou generována počítačem.
+ *   Počáteční čísla plus hráčem vyplněná čísla musí ve finále projít kontrolou, zda vyplněná hra je validní.
+ */
+public class AUvod {
+}
