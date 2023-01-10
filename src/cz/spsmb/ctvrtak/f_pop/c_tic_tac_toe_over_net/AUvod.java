d9dd0c7ffd7e2e5fb815a60119b53bfb7c7d904c
stemberk <stemberk@spsmb.cz>
2023-01-10 10:35:23 +0100

    2 pop +

---
+++ src/cz/spsmb/ctvrtak/f_pop/c_tic_tac_toe_over_net/AUvod.java
@@ -0,0 +1,9 @@
+package cz.spsmb.ctvrtak.f_pop.c_tic_tac_toe_over_net;
+
+/**
+ * Vytvořte síťovou hru piškvorky pro dva hráče. Použijte JavaFX a kontejner GridPane. Vlastní testování
+ * provádějte na jednom počítači s dvěmi instancemi hry (server, klient). Počet vítězných políček v horizontálním,
+ * vertikálním a diagonálním směru nechte na uživateli jako volitelný. Výchozí hodnota bude 3.
+ */
+public class AUvod {
+}
---
+++ src/cz/spsmb/ctvrtak/f_pop/d_maze_gen_find_best_path/AUvod.java
@@ -0,0 +1,10 @@
+package cz.spsmb.ctvrtak.f_pop.d_maze_gen_find_best_path;
+
+/**
+ * Vytvořte náhodný generátor grafu (1 počáteční uzel - vstup, N koncových uzlů - slepá cesta, 1 koncový uzel - východ).
+ * Pomocí JavaFX vykreslete tento graf v podobě bludiště. Každé rozcestí, kout, či slepý konec je reprezentován
+ * uzlem grafu.
+ * Napište metodu pro nalezení nejlepší, resp. nejkratší cesty a vykreslete jí do bludiště.
+ */
+public class AUvod {
+}
