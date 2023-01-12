72d3cf2d0ae7dbd74ce7fc648f8e0c1dd8c18b83
stemberk <stemberk@spsmb.cz>
2023-01-12 10:53:09 +0100

    + pop

---
+++ src/cz/spsmb/ctvrtak/f_pop/a_mvp_based/c_library_management/AUvod.java
@@ -0,0 +1,16 @@
+package cz.spsmb.ctvrtak.f_pop.a_mvp_based.c_library_management;
+
+/**
+ * Vytvořte aplikaci pro správu vypůjčených knih. Aplikace
+ * bude obsahovat údaje o klientovi (čtenáři) a knihách samotných.
+ * Klient:
+ * - jméno
+ * - adresa
+ *
+ * Kniha:
+ * - název
+ * - žánr
+ * - autor
+ */
+public class AUvod {
+}
