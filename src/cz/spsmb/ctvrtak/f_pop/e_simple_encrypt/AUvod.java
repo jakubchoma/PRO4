f7ae4259127823e00d4df71d6bab920a66666016
stemb <stemberk@spsmb.cz>
2023-01-16 10:39:50 +0100

    simple encrypt ˇplus

---
+++ src/cz/spsmb/ctvrtak/f_pop/e_simple_encrypt/AUvod.java
@@ -0,0 +1,14 @@
+package cz.spsmb.ctvrtak.f_pop.e_simple_encrypt;
+
+/***
+ * Vytvořte jednoduchý šifrovací algoritmus spočívající v posunu znaků o daný počet poyic v abecedě, tzv. Caesarova šifra.
+ * Vytvořte GUI v JavaFX, které bude umožňovat:
+ * a)
+ *  - vstup textu k šifrování z klávesnice i ze souboru
+ *  - výstup šifrovaného textu (na obrazovku i ze souboru)
+ *  b)
+ *  - vstup již zašifrovaného textu (možné nechat z biodu a)
+ *  - možnost zadat počet pozic k případnému rozluštění zadaného šifrovaného textu.
+ */
+public class AUvod {
+}
