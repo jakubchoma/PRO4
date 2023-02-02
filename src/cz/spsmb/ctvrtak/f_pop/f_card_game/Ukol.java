f54ad1088bdc1681a017c810a7fd735b64e9626e
stemberk <stemberk@spsmb.cz>
2023-02-02 12:16:26 +0100

    ukol

---
+++ src/cz/spsmb/ctvrtak/f_pop/f_card_game/Ukol.java
@@ -0,0 +1,43 @@
+package cz.spsmb.ctvrtak.f_pop.f_card_game;
+
+/***
+ * Vytvořte karetní hru, kde použijete výčet Cards. Vždy bude k dispozici
+ * zamíchanýá balíček karet. Katy bude možné přesouvat na cílové hromádky.
+ *
+ * Možnosti:
+ *
+ * - příklad jedna cílová hromádka, řazení pro hráče od A do 2, střídá se barva červená černá. Při přesunu karty
+ *   se score zvyšuje, při otočení na novou kartu se snižuje.
+ *
+ * - modifikace:  - hromádky 4, 8 (dva balíčky karet)
+ *                - řazení -vzestupně (A-K)
+ *                         -sestupně  (K-A)
+ *                - barvy na hromádce:  - střídání barev
+ *                                      - stejné barvy
+ *                                      - stejné druhy
+ *
+ * - další možnost je pohyb kamenu v dámě
+ * Černý Vojtěch (3)	- 4, ses, stej barvy
+ * Haken Lukáš (5)	 	- 4, vzes, stej druhy
+ * Chomanič Jakub (6)	- šachový pěšák
+ * Janošík Ondřej (7)	- dáma
+ * Karban Tadeáš (8)
+ * Kraušner Michal (10) - 8, ses, stej. druhy
+ * Krouský Robin (32)	- šachový kůň
+ * Matoušek Josef (12)	 	/	/	/
+ * Merhaut Ondřej (13)  - 8, vzestupně, stej. barva
+ * Skalický Michal (20) - šachová dáma
+ * Stanke Zdeněk (21)   - šach. střelec
+ * Šípek Pavel (23)     - 4, vzes., střídání barev
+ * Zajíček Vojtěch (29) - 4, ses, střídání barev
+ * Žárský Ondřej (31)   - 4, vzestupně, stej. barva
+ *
+ *
+ *
+ * -
+ *
+ *
+ *
+ */
+public class Ukol {
+}
