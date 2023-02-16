ddf053c3166ea9f22c989587aac70ab045fc7548
stemb <stemberk@spsmb.cz>
2023-02-16 12:55:27 +0100

    fix

--- src/cz/spsmb/ctvrtak/d_maturitni_okruhy/l_navrhove_vzory/d_navrhove_vzory_generatory_objektu/c_factory/Factory.java
+++ src/cz/spsmb/ctvrtak/d_maturitni_okruhy/l_navrhove_vzory/d_navrhove_vzory_generatory_objektu/c_factory/Factory.java
@@ -4,8 +4,8 @@ import cz.spsmb.ctvrtak.d_maturitni_okruhy.l_navrhove_vzory.d_navrhove_vzory_gen
 import cz.spsmb.ctvrtak.d_maturitni_okruhy.l_navrhove_vzory.d_navrhove_vzory_generatory_objektu.c_factory.cars.Car;
 import cz.spsmb.ctvrtak.d_maturitni_okruhy.l_navrhove_vzory.d_navrhove_vzory_generatory_objektu.c_factory.cars.Skoda;
 import cz.spsmb.ctvrtak.d_maturitni_okruhy.l_navrhove_vzory.d_navrhove_vzory_generatory_objektu.c_factory.cars.Tesla;
-// Návrhový vzor factory využívá polymorfismu, kde daná metoda (zde getCar) vrátí požadovanou instanci více
-// speifikovaného datového typu (zde rozhraní Car)
+// Návrhový vzor factory používá pomocnou metodu, (zde getCar), která vrátí požadovanou instanci více
+// specifikovaného datového typu (zde rozhraní Car). Zde se využívá polymorfismu Car může být Skoda, Bmw i Tesla.
 public class Factory {
 
     public Car getCar(String type) {
