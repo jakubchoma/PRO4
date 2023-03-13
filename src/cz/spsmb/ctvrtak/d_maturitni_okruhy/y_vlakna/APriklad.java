f2bcaf2c8b8097fada0d5ae020320eb47cb7506b
stemberk <stemberk@spsmb.cz>
2023-03-13 11:11:35 +0100

    threads zadani

---
+++ src/cz/spsmb/ctvrtak/d_maturitni_okruhy/y_vlakna/APriklad.java
@@ -0,0 +1,5 @@
+package cz.spsmb.ctvrtak.d_maturitni_okruhy.y_vlakna;
+// Vytvořte vlákno, které bude periodicky zvyšovat statickou proměnnou tmp
+// o vámi zvolenou hodnotu kačdých 200ms
+public class APriklad {
+}
