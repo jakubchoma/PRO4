5b4a660721397dc7906516223442be1958420c24
stemberk <stemberk@spsmb.cz>
2023-03-23 14:11:43 +0100

    2d hry - mat opakovani

---
+++ src/cz/spsmb/ctvrtak/d_maturitni_okruhy/w_2d_hry/AZadani.java
@@ -0,0 +1,8 @@
+package cz.spsmb.ctvrtak.d_maturitni_okruhy.w_2d_hry;
+
+/***
+ * Vytvořte dva kruhy, kde s jedním budete pohybovat pomocí kláves WASD.
+ * V případě kolize obou kruhů změňte barvu jednoho z nich.
+ */
+public class AZadani {
+}
