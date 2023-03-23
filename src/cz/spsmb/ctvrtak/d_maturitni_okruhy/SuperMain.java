b20b6904f41d6ffbadc970b140d9d12f1372e7c4
stemberk <stemberk@spsmb.cz>
2023-03-23 14:20:48 +0100

    2d hry - mat opakovani

---
+++ src/cz/spsmb/ctvrtak/d_maturitni_okruhy/SuperMain.java
@@ -0,0 +1,7 @@
+package cz.spsmb.ctvrtak.d_maturitni_okruhy;
+
+public class SuperMain {
+    public static void main(String[] args) {
+        cz.spsmb.ctvrtak.d_maturitni_okruhy.w_2d_hry.AZadani.main(args);
+    }
+}
