f6857509a1d55750e0426209d465a08d11789b1a
stemberk <stemberk@spsmb.cz>
2023-03-30 15:04:46 +0200

    2d kruhová kolize

--- src/cz/spsmb/ctvrtak/d_maturitni_okruhy/SuperMain.java
+++ src/cz/spsmb/ctvrtak/d_maturitni_okruhy/SuperMain.java
@@ -1,8 +1,11 @@
 package cz.spsmb.ctvrtak.d_maturitni_okruhy;
 
+
 public class SuperMain {
     public static void main(String[] args) {
         //cz.spsmb.ctvrtak.d_maturitni_okruhy.w_2d_hry.AZadani.main(args);
-        cz.spsmb.ctvrtak.d_maturitni_okruhy.w_2d_hry.CObdelnikovaKolize.main(args);
+        /*cz.spsmb.ctvrtak.d_maturitni_okruhy.w_2d_hry.CObdelnikovaKolize.main(args);*/
+        //cz.spsmb.ctvrtak.d_maturitni_okruhy.u_animace.AAnimationTimer.main(args);
+        cz.spsmb.ctvrtak.d_maturitni_okruhy.u_animace.BTimeline1.main(args);
     }
 }
