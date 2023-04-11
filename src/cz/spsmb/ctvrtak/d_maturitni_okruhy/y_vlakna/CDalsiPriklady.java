90aa6463fd763a25a920bb99c0667420753046f9
stemberk <stemberk@spsmb.cz>
2023-04-11 10:56:06 +0200

    vlakna

---
+++ src/cz/spsmb/ctvrtak/d_maturitni_okruhy/y_vlakna/CDalsiPriklady.java
@@ -0,0 +1,7 @@
+package cz.spsmb.ctvrtak.d_maturitni_okruhy.y_vlakna;
+/*
+Vytvořte program, který bude generovat náhodnou posloupnost se seed 1.
+Výsledky (celkem 10) pište do konzole každou sekundu.
+* */
+public class CDalsiPriklady {
+}
