8fb4dbbe97f610c006d4dc1f6568009d50366b97
stemberk <stemberk@spsmb.cz>
2023-04-13 14:16:24 +0200

    vl+

---
+++ src/cz/spsmb/ctvrtak/d_maturitni_okruhy/y_vlakna/Test.java
@@ -0,0 +1,12 @@
+package cz.spsmb.ctvrtak.d_maturitni_okruhy.y_vlakna;
+/*
+- Vytvořte program, který bude generovat náhodnou posloupnost se seed 1.
+  Výsledky (celkem 10) pište do konzole každou sekundu.
+
+- Vytvořte algoritmus, který bude generovat 3 náhodné posloupnosti odélce 10 se seed 1,
+  každou v jiném vlákně. Ve finále zobrazte součty jednotlivých posloupností
+  a celkový součet.
+
+* */
+public class Test {
+}
